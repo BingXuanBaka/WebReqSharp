601fb3f7f2ff027a006134a480ff7dd563f98733
冰轩OwO <103168301+BingXuanOwO@users.noreply.github.com>
2022-09-09 02:26:39 +0800

    Add files via upload
    上传主程序文件

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebReqSharp;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Request request = new Request()
+            {
+                Type = RequestType.POST,
+                Url = "https://www.runoob.com/try/ajax/demo_post2.php",
+                TextDatas = new List<TextData>()
+                {
+                    new TextData(){Name="fname",Context="awa"},
+                    new TextData(){Name="lname",Context="qwq"}
+                }
+            };
+            request.Send();
+            // Console.WriteLine(Encoding.UTF8.GetString(request.GetResponseBytes()));
+            Console.WriteLine(request.GetResponseString());
+        }
+    }
+}
